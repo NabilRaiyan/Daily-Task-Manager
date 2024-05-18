045a3a378c139a7498bc3bc55f1d4887188dabbb
NabilRaiyan <rraiyan77@gmail.com>
2024-05-19 01:47:53 +0600

    practicing route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "MAin Page";
+});
+
+
+Route::get("/hello", function(){
+    $num1 = 20;
+    $num2 = 30;
+    $result = $num1 + $num2;
+
+    return "Hello from another page and the result is ". $result .".";
+})->name("hello");
+
+Route::get("/greet/{name}", function($name){
+    return "Hello " . $name . " !";
+});
+
+Route::get("/nope", function(){
+    return redirect()->route('hello');
+});
+
+// if all the route is not exist then show this message
+Route::fallback(function(){
+    return "404 Not Found";
 });
