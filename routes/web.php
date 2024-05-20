3b005a3557ae2a3e70d578353e36d9386e82342d
NabilRaiyan <rraiyan77@gmail.com>
2024-05-20 10:52:40 +0600

    loading data from db

--- routes/web.php
+++ routes/web.php
@@ -4,80 +4,77 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 // Creating a task class and objects of that class
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
+// class Task
+// {
+//     public function __construct(
+//         public int $id,
+//         public string $title,
+//         public string $description,
+//         public ?string $long_description,
+//         public bool $completed,
+//         public string $created_at,
+//         public string $updated_at
+//     ) {
+//     }
+// }
 
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
+// $tasks = [
+//     new Task(
+//         1,
+//         'Buy groceries',
+//         'Task 1 description',
+//         'Task 1 long description',
+//         false,
+//         '2023-03-01 12:00:00',
+//         '2023-03-01 12:00:00'
+//     ),
+//     new Task(
+//         2,
+//         'Sell old stuff',
+//         'Task 2 description',
+//         null,
+//         false,
+//         '2023-03-02 12:00:00',
+//         '2023-03-02 12:00:00'
+//     ),
+//     new Task(
+//         3,
+//         'Learn programming',
+//         'Task 3 description',
+//         'Task 3 long description',
+//         true,
+//         '2023-03-03 12:00:00',
+//         '2023-03-03 12:00:00'
+//     ),
+//     new Task(
+//         4,
+//         'Take dogs for a walk',
+//         'Task 4 description',
+//         null,
+//         false,
+//         '2023-03-04 12:00:00',
+//         '2023-03-04 12:00:00'
+//     ),
+// ];
 
 Route::get("/", function(){
     return redirect()->route('tasks.index');
 });
 
 //  showing all the tasks
-Route::get('/tasks', function () use($tasks) {
+Route::get('/tasks', function (){
     return view('index', [
             // 'name'=>'Raiyan',
             // 'age' => 24,
-            'tasks'=> $tasks,
+            'tasks'=> \App\Models\Task::all(),
     ]);
 })->name("tasks.index");
 
 // showing single task
 
-Route::get("/tasks/{id}", function($id) use($tasks){
+Route::get("/tasks/{id}", function($id){
 
-    $task = collect($tasks)->firstWhere('id', $id);
-    if (!$task){
-        abort(Response::HTTP_NOT_FOUND);
-    }
+    $task = \App\Models\Task ::findOrFail($id); // getting specific task using id if exist
     return view('show', ['task'=>$task]);
 })->name("single-task.show");
 
