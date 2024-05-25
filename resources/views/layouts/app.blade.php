d934e17254f2b592fb55d0174d0a554c0ee1715a
NabilRaiyan <rraiyan77@gmail.com>
2024-05-25 09:14:17 +0600

    adding flash message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,17 @@
             @if (session()->has('success'))
                 <div class="text-green-800">{{session('success')}}</div>
             @endif -->
-            <div class="bg-green-100 p-3 border border-green-600 text-green-800 rounded w-4/12 mx-auto">
+            <div class="relative bg-green-100 p-3 border border-green-600 text-green-800 rounded w-4/12 mx-auto" role="alert">
                 <strong class="font-bold">Success!</strong>
                 <div class="text-base">This is a flash message!</div>
+
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" @click="flash = false"
+                        stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
             </div>
             @yield('content')
         </div>
