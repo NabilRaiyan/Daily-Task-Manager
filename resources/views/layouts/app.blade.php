b0d622360911efa0e97c4d3017e60ed072145404
NabilRaiyan <rraiyan77@gmail.com>
2024-05-25 09:26:04 +0600

    adding flash message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,23 +15,22 @@
 <body class="container mt-10 mb-10 mx-auto">
     <div>
         <h1 class="mt-5 mb-5 text-2xl text-gray-700">@yield('title')</h1>
-        <div class="mt-5 mb-5 text-xl">
-            <!-- showing flash message
+        <div class="mt-5 mb-5 text-xl" x-data="{flash:true}">
+            <!-- showing flash message -->
             @if (session()->has('success'))
-                <div class="text-green-800">{{session('success')}}</div>
-            @endif -->
-            <div class="relative bg-green-100 p-3 border border-green-600 text-green-800 rounded w-4/12 mx-auto" role="alert">
-                <strong class="font-bold">Success!</strong>
-                <div class="text-base">This is a flash message!</div>
+                <div x-show="flash" class="relative bg-green-100 p-3 border border-green-600 text-green-800 rounded w-4/12 mx-auto" role="alert">
+                    <strong class="font-bold">Success!</strong>
+                    <div class="text-base">{{session('success')}}!</div>
 
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" @click="flash = false"
-                        stroke="currentColor" class="h-6 w-6 cursor-pointer">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </span>
-            </div>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" @click="flash =false"
+                            stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </span>
+                </div>
+            @endif
             @yield('content')
         </div>
     </div>
